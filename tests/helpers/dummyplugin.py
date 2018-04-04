24a9d37c26b6764b7787a02cfb4d3517b804d889
bytinbit <meline.sieber@hsr.ch>
2018-04-04 12:00:58 +0200

    dummyplugin finalized, tests completed

--- tests/helpers/dummyplugin.py
+++ tests/helpers/dummyplugin.py
@@ -1,29 +1,31 @@
-"""Dummy plugin so that we can separately test kitovu and the plugin architecture."""
+"""Dummy plugin so that we can test the plugin architecture and the rest of kitovu separately."""
 
 import attr
 from kitovu.sync import syncplugin
 import pathlib
 import typing
 
+
 @attr.s
 class Digests:
 
-    """test-class that provies the local and remote digests as strings"""
+    """test-class that provides the local and remote digests as strings"""
 
     local_digest: str = attr.ib()
     remote_digest: str = attr.ib()
 
+
 @attr.s
 class DummyPlugin(syncplugin.AbstractSyncPlugin):
 
-    """provides fake connection info with hard-coded credentials for testing."""
+    """provides a fake connection with hard-coded filenames for testing."""
 
-    paths: typing.Dict[pathlib.PurePath, Digests] = attr.ib({
+    paths: typing.Dict[pathlib.PurePath, Digests] = attr.ib(attr.Factory(lambda: {
         pathlib.PurePath("example1.txt"): Digests("1", "1"),
         pathlib.PurePath("example2.txt"): Digests("2", "2"),
         pathlib.PurePath("example3.txt"): Digests("3", "3"),
         pathlib.PurePath("example4.txt"): Digests("4", "4"),
-    })
+    }))
     connection_state: bool = attr.ib(False)
 
     def configure(self, info: typing.Dict[str, typing.Any]) -> None:
@@ -31,12 +33,10 @@ class DummyPlugin(syncplugin.AbstractSyncPlugin):
 
     def connect(self) -> None:
         self.connection_state = True
-        print("connection established")
 
     def disconnect(self) -> None:
         assert self.connection_state
         self.connection_state = False
-        print("connection closed")
 
     def create_local_digest(self, path: pathlib.PurePath) -> str:
         assert self.connection_state
@@ -52,6 +52,7 @@ class DummyPlugin(syncplugin.AbstractSyncPlugin):
             yield n
 
     def retrieve_file(self, path: pathlib.PurePath, fileobj: typing.IO[bytes]) -> None:
+        # for the sake of testing, it writes gibberish into the file
         assert self.connection_state
         remote_digest = self.paths[path].remote_digest
         fileobj.write(f"{path}\n{remote_digest}")
--- tests/test_dummyplugin.py
+++ tests/test_dummyplugin.py
@@ -1,7 +1,8 @@
 import kitovu.utils
+import os
 import pathlib
 import pytest
-
+import typing
 from tests.helpers import dummyplugin
 
 
@@ -40,11 +41,46 @@ def test_remote_digest(plugin):
     assert remote_digest == "1"
 
 
-def test_local_digest_changed(plugin):
+def test_remote_digest_changed(plugin):
     plugin.connect()
     plugin.paths[pathlib.PurePath("example1.txt")].remote_digest = "42"
     remote_digest= plugin.create_remote_digest(pathlib.PurePath("example1.txt"))
     assert remote_digest == "42"
 
 
+def test_if_list_path_lists_correct_pathnames(plugin):
+    pathnames: typing.Iterable[pathlib.PurePath] = [pathlib.PurePath("example1.txt"),
+                                                    pathlib.PurePath("example2.txt"),
+                                                    pathlib.PurePath("example3.txt"),
+                                                    pathlib.PurePath("example4.txt")]
+    plugin.connect()
+    all_paths = list(plugin.list_path(plugin.paths))
+    assert all_paths == pathnames
+
+
+def test_if_correct_file_retrieved(plugin):
+    filename = "testsample.txt"
+    if os.path.exists(filename):
+        os.remove(filename)
+    plugin.connect()
+    f = open(filename, "w")
+    with f:
+        plugin.retrieve_file(pathlib.PurePath("example1.txt"), f)
+    f = open(filename, "r")
+    with f:
+        assert f.read() == "example1.txt\n1"
+
+
+def test_if_changed_digest_still_retrieves_correct_file(plugin):
+    filename = "testsample.txt"
+    if os.path.exists(filename):
+        os.remove(filename)
+    plugin.connect()
+    plugin.paths[pathlib.PurePath("example1.txt")].remote_digest = "42" # change remote digest
+    f = open(filename, "w")
+    with f:
+        plugin.retrieve_file(pathlib.PurePath("example1.txt"), f)
+    f = open(filename, "r")
+    with f:
+        assert f.read() == "example1.txt\n42"
 
