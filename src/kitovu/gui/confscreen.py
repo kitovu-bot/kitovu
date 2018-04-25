5d8ffd3495f9c6d363e2fe934969bac8fbf170c3
Florian Bruhin <git@the-compiler.org>
2018-04-25 16:37:44 +0200

    GUI: Make config editing work

--- src/kitovu/gui/confscreen.py
+++ src/kitovu/gui/confscreen.py
@@ -1,16 +1,40 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit
+import functools
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QDialogButtonBox
+from PyQt5.QtCore import pyqtSignal
 
 from kitovu.sync import settings
 
 
 class ConfScreen(QWidget):
 
+    close_requested = pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self._vbox = QVBoxLayout(self)
         self._edit = QTextEdit()
+        self._edit.setStyleSheet("font-family: monospace;")
         self._vbox.addWidget(self._edit)
 
-        conf_file = settings.get_config_file_path()
-        self._edit.setText(conf_file.read_text('utf-8'))
+        self._conf_file = settings.get_config_file_path()
+        self._edit.setPlainText(self._conf_file.read_text('utf-8'))
+        self._edit.setAcceptRichText(False)
+
+        self._buttons = QDialogButtonBox()
+        self._cancel_button = self._buttons.addButton("Abbrechen", QDialogButtonBox.DestructiveRole)
+        self._save_button = self._buttons.addButton("Speichern", QDialogButtonBox.ApplyRole)
+        self._back_button = self._buttons.addButton("Speichern und zurück", QDialogButtonBox.AcceptRole)
+        self._vbox.addWidget(self._buttons)
+
+        self._cancel_button.clicked.connect(self.close_requested)
+        self._save_button.clicked.connect(functools.partial(self.save, close=False))
+        self._back_button.clicked.connect(functools.partial(self.save, close=True))
+
+    def save(self, close):
+        text = self._edit.toPlainText()
+        # FIXME validate config and show errors
+        self._conf_file.write_text(text, encoding='utf-8')
+        if close:
+            self.close_requested.emit()
--- src/kitovu/gui/mainwindow.py
+++ src/kitovu/gui/mainwindow.py
@@ -16,10 +16,13 @@ class CentralWidget(QStackedWidget):
 
         self._start_screen = startscreen.StartScreen()
         self.addWidget(self._start_screen)
+
         self._start_screen.sync_pressed.connect(
             lambda: self.setCurrentWidget(self._sync_screen))
         self._start_screen.conf_pressed.connect(
             lambda: self.setCurrentWidget(self._conf_screen))
+        self._conf_screen.close_requested.connect(
+            lambda: self.setCurrentWidget(self._start_screen))
 
         self.setCurrentWidget(self._start_screen)
 
