f5f8bd0c72f1c9f427b2b204268625e204cbc594
Nicolas Ganz <nicolas@keltec.ch>
2018-05-12 18:35:12 +0200

    fixed tests

--- tests/sync/test_smb.py
+++ tests/sync/test_smb.py
@@ -54,12 +54,12 @@ class SMBConnectionMock:
         self.connected_port = None
 
     def getAttributes(self, share, path):
-        if path.basename == 'missing':
+        if path.endswith('missing'):
             raise OperationFailure('msg1', 'msg2')
         return self.AttributesMock(1024, 988824605.56)
 
     def listPath(self, share, path):
-        if path.basename == 'missing':
+        if path.endswith('missing'):
             raise OperationFailure('msg1', 'msg2')
         if str(path).endswith('example_dir') or str(path).endswith('sub'):
             return [self.SharedFileMock('sub_file', False)]
