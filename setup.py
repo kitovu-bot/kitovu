c8795e8a47aceef2b7d1acf10f2582352663e080
Lukas Röllin <lsroellin@gmail.com>
2018-09-18 19:15:59 +0200

    Remove example extras_requires

--- setup.py
+++ setup.py
@@ -69,9 +69,6 @@ setup(
     ],
     extras_require={
         'gui': ['PyQt5']
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
     },
     entry_points={
         'console_scripts': [
